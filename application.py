1c739d99a7a388e547b7aa43d7b04d68e24508b5
Adam Trotta <ahtrotta@gmail.com>
2020-01-27 16:34:41 -0500

    added read page and read function

--- application.py
+++ application.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["POST", "GET"])
 def index():
     if request.method == 'GET':
         return render_template('index.html')
 
     if request.method == 'POST':
         return render_template('success.html')
+
+@app.route('/read')
+def read():
+    return render_template('read.html')
