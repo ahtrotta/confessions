fad7263e352901f0218fb7ccc06a3141e44d598d
Adam Trotta <ahtrotta@gmail.com>
2020-01-27 16:00:57 -0500

    added success page, modified application.py

--- application.py
+++ application.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    if request.method == 'GET':
+        return render_template('index.html')
+
+    if request.method == 'POST':
+        return render_template('success.html')
