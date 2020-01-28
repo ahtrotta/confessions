3b2863b3f1316a4370e20cbb6b8dd9527cac00e6
ahtrotta <ahtrotta@gmail.com>
2020-01-27 22:14:26 -0500

    added view page

--- application.py
+++ application.py
@@ -1,17 +1,22 @@
 from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
-@app.route('/', methods=["POST", "GET"])
+@app.route('/', methods=['POST', 'GET'])
 def index():
+    
     if request.method == 'GET':
         return render_template('index.html')
 
-    if request.method == 'POST':
-        
+    elif request.method == 'POST':
         confession = request.form.get("confession")
-
         return render_template('success.html', confession=confession)
 
 @app.route('/read')
 def read():
-    return render_template('read.html')
+    
+    if request.method == 'GET':
+        return render_template('read.html')
+    
+    elif request.method == 'POST':
+        return render_template('view.html', secret="secret")
