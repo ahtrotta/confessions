2fbef8de0eca03bd4b71317774ece5d379a4430d
Adam Trotta <ahtrotta@gmail.com>
2020-02-14 17:45:30 -0500

    added requirements and modified __init__

--- confessions/__init__.py
+++ confessions/__init__.py
@@ -1,21 +1,30 @@
 import os
 
-from flask import Flask, g
-from flask_session import Session
-from flask_redis import FlaskRedis
+from flask import Flask, g, session
+from Flask-Session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-# Globally accessible libraries
-# db = 
-r = FlaskRedis()
-
-
 def create_app(test_config=None):
-    """Initialize the core application."""
+    # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'confessions.sqlite')
+        DATABASE='placeholder'#PATH TO DATABASE
     )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+
+
 
