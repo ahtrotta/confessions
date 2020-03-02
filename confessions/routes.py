328492433dd795325791bde8e22a31368448bc4c
Adam Trotta <ahtrotta@gmail.com>
2020-03-02 15:57:46 -0500

    added comments to routes.py

--- confessions/routes.py
+++ confessions/routes.py
@@ -9,6 +9,7 @@ from random import choice
 def index():
     return render_template('about.html')
 
+
 @app.route('/confess', methods=['POST', 'GET'])
 def confess():
     
@@ -16,11 +17,19 @@ def confess():
         return render_template('confess.html')
 
     elif request.method == 'POST':
+        
+        # retrieve confession from form
         confession = request.form.get("confession")
+        
+        # store in Confession database model
         new_confession = Confession(confession=confession, created=datetime.now())
+        
+        # add to database
         db.session.add(new_confession)
         db.session.commit()
+
         return render_template('success.html', confessions=db.session.query(Confession).all())
+
 
 @app.route('/read', methods=['POST', 'GET'])
 def read():
@@ -29,12 +38,20 @@ def read():
         return render_template('read.html')
     
     elif request.method == 'POST':
+        # retrieve all ids in the database
         ids = db.session.query(Confession.id).all()
         id_list = []
         for id, in ids:
             id_list.append(id)
+        
+        # randomly select one id
         rand_id = choice(id_list)
+
+        # retrieve random secret
         secret = db.session.query(Confession).filter(Confession.id == rand_id).all()
+        
+        # delete retrieved secret
         db.session.delete(secret[0])
         db.session.commit()
+
         return render_template('view.html', secret=secret[0].confession)
